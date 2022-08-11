1896bbb55c51e62d17ded5a3855cfa046d3745a4
Jose Miguel Erazo Ayón <46788386+josemerazo@users.noreply.github.com>
2022-08-11 12:02:20 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,5 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "practica_leng_prog";
---
+++ formulario.php
@@ -0,0 +1,10 @@
+<?php
+include_once("conexion.php");
+include_once("head.php");
+?>
+
+<h1>Creaciòn de formulario</h1>
+
+<?php
+include_once("footer.php");
+?>
